4d0758bcd473ca0ab50948273048360b0c3f0abe
chato-cebes <javancebes@gmail.com>
2023-04-01 03:45:51 -0300

    delete soft1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { usePostUserMutation } from "./app/api/properties";
-import Landing from "./pages/Landing";
-import Home from "./pages/Home";
+import { setUser } from "./app/slices/logUser";
+import Comments from "./pages/Comments";
 import Detail from "./pages/Detail";
-import Search from "./pages/Search";
+import Estadistica from "./pages/Estadistica";
 import Filter from "./pages/Filter";
 import FilterCombine from "./pages/FilterCombine";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
 import PropertiesForm from "./pages/PropertiesForm/PropertiesForm";
 import Step1 from "./pages/PropertiesForm/Step1";
@@ -19,12 +22,8 @@ import Step6 from "./pages/PropertiesForm/Step6";
 import Step7 from "./pages/PropertiesForm/Step7";
 import Step8 from "./pages/PropertiesForm/Step8";
 import Step9 from "./pages/PropertiesForm/Step9";
-import { setUser } from "./app/slices/logUser";
-import { useSelector, useDispatch } from "react-redux";
-import Comments from "./pages/Comments";
-import Estadistica from "./pages/Estadistica";
 import PublicacionesUsuario from "./pages/PublicacionesUsuario";
-
+import Search from "./pages/Search";
 
 const App = () => {
   const { logUser: globalUser } = useSelector((state) => state.logUser);
@@ -51,30 +50,30 @@ const App = () => {
 
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Landing/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/detail/:id" element={<Detail/>}/>
-      <Route path="/search/:filter" element={<Search/>}/>
-      <Route path="/filter/:filter" element={<Filter/>}/>
-      <Route path="/filterCombine" element={<FilterCombine/>}/>
-      <Route path="/addproperty" element={<PropertiesForm/>}/>
-      <Route path="/addproperty/step1" element={<Step1/>}/>
-      <Route  path="/addproperty/step2" element={<Step2/>}/>
-      <Route path="/addproperty/step3" element={<Step3/>}/>
-      <Route path="/addproperty/step4" element={<Step4/>}/>
-      <Route path="/addproperty/step5" element={<Step5/>}/>
-      <Route path="/addproperty/step6" element={<Step6/>}/>
-      <Route path="/addproperty/step7" element={<Step7/>}/>
-      <Route path="/addproperty/step8" element={<Step8/>}/>
-      <Route path="/addproperty/step9" element={<Step9/>}/>
-      <Route path="/dashboard" element={<Profile/>}/>
-      <Route path="/comments" element={<Comments/>}/>
-      <Route path="/stats" element={<Estadistica/>}/>
-      <Route path="/mypublic" element={<PublicacionesUsuario/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/search/:filter" element={<Search />} />
+          <Route path="/filter/:filter" element={<Filter />} />
+          <Route path="/filterCombine" element={<FilterCombine />} />
+          <Route path="/addproperty" element={<PropertiesForm />} />
+          <Route path="/addproperty/step1" element={<Step1 />} />
+          <Route path="/addproperty/step2" element={<Step2 />} />
+          <Route path="/addproperty/step3" element={<Step3 />} />
+          <Route path="/addproperty/step4" element={<Step4 />} />
+          <Route path="/addproperty/step5" element={<Step5 />} />
+          <Route path="/addproperty/step6" element={<Step6 />} />
+          <Route path="/addproperty/step7" element={<Step7 />} />
+          <Route path="/addproperty/step8" element={<Step8 />} />
+          <Route path="/addproperty/step9" element={<Step9 />} />
+          <Route path="/dashboard" element={<Profile />} />
+          <Route path="/comments" element={<Comments />} />
+          <Route path="/stats" element={<Estadistica />} />
+          <Route path="/mypublic" element={<PublicacionesUsuario />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
